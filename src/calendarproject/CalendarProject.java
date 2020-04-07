97f60a719913cb7234bd7bffbe73484773670426
DruidUnderscoreBoy <61715465+DruidUnderscoreBoy@users.noreply.github.com>
2020-04-07 11:08:36 -0400

    Demo 1.0

---
+++ src/calendarproject/CalendarProject.java
@@ -0,0 +1,42 @@
+package calendarproject;
+import javax.swing.JFrame;
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.JInternalFrame;
+
+public final class CalendarProject extends JFrame {
+    
+    //Height and width used by main window size
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 800;
+
+protected Component calendar(){ //This component creates an internal frame for MonthlyCalendar.java
+    MonthlyCalendar.run();
+    JInternalFrame frame = new JInternalFrame("",false,false,false,false);
+    frame.add(MonthlyCalendar.run());
+    frame.setSize(HEIGHT,WIDTH);
+    frame.setVisible(true);
+    frame.setResizable(false);
+    return frame;
+}
+
+public CalendarProject()
+{
+    //basic stuff for the window
+    setTitle("Calendar Project");
+    setSize(WIDTH,HEIGHT);
+    setVisible(true);
+    setResizable(false);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    Container cont = getContentPane();
+    Component panel2 = calendar();
+    cont.add(panel2);
+    
+}
+
+//    public static void main(String[] args){
+//        //Actually run the program
+//        CalendarProject yeet = new CalendarProject(); 
+//    }
+}
---
+++ src/calendarproject/DataHandler.java
@@ -0,0 +1,67 @@
+package calendarproject;
+
+import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+
+public class DataHandler {
+    DataHandler use = new DataHandler();
+
+    public static HashSet<String> eventList = new HashSet<String>();
+
+    public static void addToList(int type, int userType, int userID, int year, int month, int day, String eventName, int startTime, int endTime) throws IOException{
+        //the first constructor: type is to differenciate if an event happens once or on a certain interval
+        //0 for one time
+        //1 for weekly
+        //2 for monthly
+        //3 for yearly
+        //and most likely 4 will be a custom option
+        
+        //for usertype, 0 is regular user, 1 is group leader
+        
+        eventList.add(type + "," + userType + "," + userID + "," + year + "," + month + "," + day + "," + eventName + "," + startTime + "," + endTime);
+        //Reminder: The calendar counts January as month 0, and December as month 11
+        write();
+        //When you put an event in, it writes it to a text file. 
+        read();
+        //Reads from text file.  
+        System.out.println(Arrays.toString(eventList.toArray()));
+        //for testing purposes
+    }
+    //puts events into txt file
+    public static void write() throws IOException{
+            try
+            {
+                FileWriter text = new FileWriter("calendar.txt", true);
+                BufferedWriter writer = new BufferedWriter(text);
+                for (String str : eventList)
+                {
+                    writer.write(str + System.lineSeparator());
+                }
+                writer.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println("");
+            }
+                
+        }
+    //Reads from file and puts it into the ArrayList
+    public static void read() throws IOException{
+            Scanner reader = null;
+            try
+            {
+                reader = new Scanner(new File("calendar.txt")).useDelimiter("\\n");
+            }
+            catch ( FileNotFoundException ex)
+            {
+                System.out.println(ex+"  file not found ");
+            }
+            while (reader.hasNext())
+            {
+                String str = reader.nextLine();
+                eventList.add(str);
+            }
+            reader.close();
+    }
+}
